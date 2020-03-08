50339c41d1d8e261a9d2590af9cf10b1f14661d6
maimonah95 <mim95s@hotmail.com>
2020-03-08 09:23:48 +0300

    add Insertion sort

---
+++ sort.js
@@ -0,0 +1,17 @@
+
+
+//Insertion sort
+const insertion = (nums) => {
+  for (let i = 1; i < nums.length; i++) {
+    let j = i - 1;
+    let temp = nums[i];
+    while (j >= 0 && nums[j] > temp) {
+      nums[j + 1] = nums[j];
+      j--;
+    }
+    nums[j + 1] = temp;
+  }
+  return nums;
+};
+console.log(insertion([3,0,2,5]))
+
