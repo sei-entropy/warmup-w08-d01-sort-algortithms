5830411488229d628219087db826aab4916ded83
maimonah95 <mim95s@hotmail.com>
2020-03-08 10:02:36 +0300

    add Quicksort

--- sort.js
+++ sort.js
@@ -17,13 +17,13 @@ console.log(insertion([3,0,2,5]))
 
 //bubble
 function bubble(a) {
-  var swapp;
-  var n = a.length - 1;
+  let swapp;
+  let n = a.length - 1;
   do {
     swapp = false;
     for (var i = 0; i < n; i++) {
       if (a[i] < a[i + 1]) {
-        var temp = a[i];
+        let temp = a[i];
         a[i] = a[i + 1];
         a[i + 1] = temp;
         swapp = true;
@@ -34,4 +34,46 @@ function bubble(a) {
   return a;
 }
 console.log(bubble([12, 7, 3, 9, 15]));
+//Quicksort
+let items = [5,3,7,6,2,9];
+function swap(items, leftIndex, rightIndex){
+    let temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+    let pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+function quickSort(items, left, right) {
+    let index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+// first call to quick sort
+let sortedArray = quickSort(items, 0, items.length - 1);
+console.log(sortedArray); //prints [2,3,5,6,7,9]
 
