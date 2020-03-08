91f4e9c49e93aeb4fe11d79fdf40ad26b29687a5
maimonah95 <mim95s@hotmail.com>
2020-03-08 09:55:26 +0300

    add bubble sort

--- sort.js
+++ sort.js
@@ -1,17 +1,37 @@
 
 
 //Insertion sort
-const insertion = (nums) => {
-  for (let i = 1; i < nums.length; i++) {
+const insertion = (a) => {
+  for (let i = 1; i < a.length; i++) {
     let j = i - 1;
-    let temp = nums[i];
-    while (j >= 0 && nums[j] > temp) {
-      nums[j + 1] = nums[j];
+    let temp = a[i];
+    while (j >= 0 && a[j] > temp) {
+      a[j + 1] = a[j];
       j--;
     }
-    nums[j + 1] = temp;
+    a[j + 1] = temp;
   }
-  return nums;
+  return a;
 };
 console.log(insertion([3,0,2,5]))
+
+//bubble
+function bubble(a) {
+  var swapp;
+  var n = a.length - 1;
+  do {
+    swapp = false;
+    for (var i = 0; i < n; i++) {
+      if (a[i] < a[i + 1]) {
+        var temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+        swapp = true;
+      }
+    }
+    n--;
+  } while (swapp);
+  return a;
+}
+console.log(bubble([12, 7, 3, 9, 15]));
 
